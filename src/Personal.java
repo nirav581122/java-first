d4686302fa4e30ce2e4ef999204850cd3db16cad
nirav5811 <niravnamlee@gmail.com>
2023-03-17 23:40:26 +0000

    personal  programme has been done

---
+++ src/Personal.java
@@ -0,0 +1,20 @@
+public class Personal {
+    static String name ="Sam";
+    static int age= 25;
+    static int mobileNumber =342584555;
+    static String address = "3 ravi avenue AB2 BF3";
+    static  char grade ='A';
+    static boolean eligibilty= true;
+    static  double amount =10.36;
+
+    public static void main(String[] args) {
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(mobileNumber);
+        System.out.println(address);
+        System.out.println(grade);
+        System.out.println(eligibilty);
+        System.out.println(amount);
+    }
+
+}
